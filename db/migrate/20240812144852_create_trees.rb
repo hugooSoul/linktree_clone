419462f69f13c8a733608fcd4d3c95da032f8e31
hugooSoul <mandujano@Hugos-MacBook-Pro.local>
2024-08-12 09:20:07 -0600

    Initial commit + devise added

---
+++ db/migrate/20240812144852_create_trees.rb
@@ -0,0 +1,15 @@
+class CreateTrees < ActiveRecord::Migration[7.1]
+  def change
+    create_table :trees do |t|
+      t.string :name
+      t.string :x
+      t.string :instagram
+      t.string :youtube
+      t.string :tiktok
+      t.integer :user_id
+      t.string :style
+
+      t.timestamps
+    end
+  end
+end
